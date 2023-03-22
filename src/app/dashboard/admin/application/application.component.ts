171d43eacc7b2011ad2a0600a1223b3a6f735e60
techoalok <akokkumarsingh862@gmail.com>
2023-03-22 15:54:34 +0530

    alok(tables all)

--- src/app/dashboard/admin/application/application.component.ts
+++ src/app/dashboard/admin/application/application.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./application.component.css']
 })
 export class ApplicationComponent {
-
+  displayedColumns: string[] = ['position', 'id', 'fname', 'mname','email','address','mobile','proffession', 'action'];
+  dataSource = ELEMENT_DATA;
 }
+export interface PeriodicElement {
+  fname: string;
+  position: number;
+  id: number;
+  mname: string;
+  action:string;
+  email:string;
+  address:string;
+  mobile:number;
+  proffession:string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, id:101, fname: 'Nakul Singh', mname: 'Rani Kumari',email: 'alokdhhjfi@gmail.com', address:'hajipur', mobile:98012554512, proffession:'farmer', action:'none' },
+  {position: 1, id:101, fname: 'Nakul Singh', mname: 'Rani Kumari',email: 'alokdhhjfi@gmail.com', address:'hajipur', mobile:98012554512, proffession:'farmer', action:'none' },
+  {position: 1, id:101, fname: 'Nakul Singh', mname: 'Rani Kumari',email: 'alokdhhjfi@gmail.com', address:'hajipur', mobile:98012554512, proffession:'farmer', action:'none' },
+  {position: 1, id:101, fname: 'Nakul Singh', mname: 'Rani Kumari',email: 'alokdhhjfi@gmail.com', address:'hajipur', mobile:98012554512, proffession:'farmer', action:'none' },
+  {position: 1, id:101, fname: 'Nakul Singh', mname: 'Rani Kumari',email: 'alokdhhjfi@gmail.com', address:'hajipur', mobile:98012554512, proffession:'farmer', action:'none' },
+  
+];
