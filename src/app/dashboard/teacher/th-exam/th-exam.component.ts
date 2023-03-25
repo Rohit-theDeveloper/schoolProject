f7c2080a557d105a599b29596d2ed109b4b70d98
Aditech83 <srivastavaraj531@gmail.com>
2023-03-25 22:42:02 +0530

    question-section

--- src/app/dashboard/teacher/th-exam/th-exam.component.ts
+++ src/app/dashboard/teacher/th-exam/th-exam.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-
-interface Food {
-  value: string;
-  viewValue: string;
-}
 interface classs {
   value: string;
   viewValue: string;
 }
 interface subject {
-  value: string;
-  viewValue: string;
-}
-interface question {
   value: string;
   viewValue: string;
 }
@@ -22,11 +13,6 @@ interface question {
   styleUrls: ['./th-exam.component.css']
 })
 export class ThExamComponent {
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-  ];
   classes: classs[] = [
     {value: 'play-0', viewValue: 'Play'},
     {value: 'Nurs-1', viewValue: 'Nurs'},
@@ -43,12 +29,6 @@ export class ThExamComponent {
     {value: 'GK-4', viewValue: 'GK'},
     {value: 'Computer-5', viewValue: 'Computer'},
   ];
-  questions: question[] = [
-    {value: '10-0', viewValue: '10'},
-    {value: '20-1', viewValue: '20'},
-    {value: '30-2', viewValue: '30'},
-    {value: '40-3', viewValue: '40'},
-    {value: '50-4', viewValue: '50'},
-  ];
+  
 }
 
