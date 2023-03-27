3027c8585b590e2f6cc8300cac1e9aca547dad94
ideal-ewb-001 <adarsh001raj@gmail.com>
2023-03-27 12:56:03 +0530

    adarsh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,6 @@ import { StudentComponent } from './dashboard/student/student.component';
 import { LibrarianComponent } from './dashboard/librarian/librarian.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { ParentComponent } from './dashboard/admin/parent/parent.component';
 import { StaffComponent } from './dashboard/admin/staff/staff.component';
 import { AdminDashboardComponent } from './dashboard/admin/admin-dashboard/admin-dashboard.component';
 import { ClassComponent } from './dashboard/admin/class/class.component';
@@ -57,7 +56,6 @@ import { ThLibrarydetailsComponent } from './dashboard/teacher/th-librarydetails
 import { ThExamComponent } from './dashboard/teacher/th-exam/th-exam.component';
 import { ThApplicationComponent } from './dashboard/teacher/th-application/th-application.component';
 import { ThNotificationComponent } from './dashboard/teacher/th-notification/th-notification.component';
-import { AddParentComponent } from './dashboard/admin/parent/add-parent/add-parent.component';
 import { ManageStudentComponent } from './dashboard/admin/admin-student/manage-student/manage-student.component';
 import { ManageTeacherComponent } from './dashboard/admin/admin-teacher/manage-teacher/manage-teacher.component';
 import { ManageApplicationComponent } from './dashboard/admin/application/manage-application/manage-application.component';
@@ -100,7 +98,6 @@ import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
     AdminComponent,
     StudentComponent,
     LibrarianComponent,
-    ParentComponent,
     StaffComponent,
     AdminDashboardComponent,
     ClassComponent,
@@ -132,7 +129,6 @@ import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
     ThExamComponent,
     ThApplicationComponent,
     ThNotificationComponent,
-    AddParentComponent,
     ManageStudentComponent,
     ManageTeacherComponent,
     ManageApplicationComponent,
