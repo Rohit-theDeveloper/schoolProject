ed66e2a94ec1b5f800b4118660b4c941f15704bf
Rohit-theDeveloper <121475091+Rohit-theDeveloper@users.noreply.github.com>
2023-04-04 12:16:44 +0530

    sdw

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,13 +10,16 @@ export class ApiService {
   constructor(
     private http:HttpClient
   ) { }
-
+  // book getting data from data bas
   get_book(){
     return this.http.get(this.baseUrl+'bookdataget.php');
   }
   post_book(data:any){
     return this.http.post(this.baseUrl + 'booksforminsert.php',data);
   }
+
+  // endingg......
+
   // library geting data from database
   get_library(){
     return this.http.get(this.baseUrl+'libraryget.php');
@@ -24,35 +27,44 @@ export class ApiService {
   post_library(data:any){
     return this.http.post(this.baseUrl + 'libraryinsert.php',data);
   }
-  //book transaction database api
+  // endingg......
 
+  //book transaction database api
   get_book_tran(){
     return this.http.get(this.baseUrl+'booktransactionget.php');
   }
   post_book_tran(data:any){
     return this.http.post(this.baseUrl + 'booktransactioninsert.php',data);
   }
+    // endingg......
+
   //application database api
   get_librnapplication(){
     return this.http.get(this.baseUrl+'view_librnapplication.php');
   }
+  // endingg......
 
+  //student getting database api
   get_student(){
     return this.http.get(this.baseUrl+'std_view.php');
   }
   post_std(data:any){
     return this.http.post(this.baseUrl+'std_add.php',data);
   }
+
+  // endingg......
+
   get_staff(){
     return this.http.get(this.baseUrl+'staff_view.php');
   }
-
+//student getting database api
   get_fee(){
     return this.http.get(this.baseUrl+'fee_recp_view.php');
   }
   get_notice(){
     return this.http.get(this.baseUrl+"notification_view_tbl.php");
   }
+   // endingg......
 }
 
 
