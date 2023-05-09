8c8a0ea7c57488a9ed5ac556fe877fd511618574
techoalok <akokkumarsingh862@gmail.com>
2023-05-09 11:57:06 +0530

    respostd

--- src/app/dashboard/admin/admin.component.ts
+++ src/app/dashboard/admin/admin.component.ts
@@ -10,9 +10,12 @@ export class AdminComponent implements OnInit {
   autoside:boolean = true
   login_details:any
   login_as:any 
+  sidemode:any ='side'
   ngOnInit(): void {
     if(window.innerWidth<768){
       this.autoside=false
+      this.sidemode='over'
+
     }
     this.login_details=localStorage.getItem('token')
     this.login_as=JSON.parse(this.login_details)
